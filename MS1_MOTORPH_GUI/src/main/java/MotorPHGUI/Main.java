4ea0ec79a90baec31565e8a52b4e5981805ff74c
John Carlo Rendon <lr.jcrendon@mmdc.mcl.edu.ph>
2025-06-04 22:46:32 +0800

    Add files via upload

---
+++ MS1_MOTORPH_GUI/src/main/java/MotorPHGUI/Main.java
@@ -0,0 +1,14 @@
+package MotorPHGUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+                // Launch the LoginDashboard
+        java.awt.EventQueue.invokeLater(() -> {
+            LoginDashboard login = new LoginDashboard();
+            login.setVisible(true);
+        });
+    }
+}
+        
+        
